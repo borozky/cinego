cd2e3b0f629ee3175a646c69e2f5ac55987a43d7
Victor Orosco <victororosco@victoroscosiMac.gateway>
2017-07-23 22:37:55 +1000

    First commit

---
+++ cinego/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  cinego
+//
+//  Created by Victor Orosco on 23/7/17.
+//  Copyright © 2017 ISE Superstars. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
