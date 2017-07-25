82859eb27bbd4fa693bd23192e707570c207e2a2
Joshua Orozco <s3485376@b01005001-06.local>
2017-07-25 10:40:58 +1000

    Updated the scroll view on the home page

---
+++ cinego/HomeViewController.swift
@@ -0,0 +1,40 @@
+//
+//  HomeViewController.swift
+//  cinego
+//
+//  Created by Joshua Orozco on 7/25/17.
+//  Copyright © 2017 ISE Superstars. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var upcomingMoviesLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        upcomingMoviesLabel.text = "Hello World"
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ cinego/Temp.h
@@ -0,0 +1,13 @@
+//
+//  Temp.h
+//  cinego
+//
+//  Created by Joshua Orozco on 7/25/17.
+//  Copyright © 2017 ISE Superstars. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Temp : NSObject
+
+@end
