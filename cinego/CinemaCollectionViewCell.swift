32f67e4c854c9e9d9bbdbf7c1f1ff5927df4fcbd
Joshua Orozco <s3485376@student.rmit.edu.au>
2017-07-29 14:50:59 +1000

    Cinema theaters carousel on home page

--- cinego/CinemaCollectionViewCell.swift
+++ cinego/CinemaCollectionViewCell.swift
@@ -10,5 +10,14 @@ import UIKit
 
 class CinemaCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var cinemaImage: UIImageView!
+    @IBOutlet weak var cinemaViewFX: UIVisualEffectView!
+    @IBOutlet weak var cinemaLabelParent: UIView!
+    @IBOutlet weak var cinemaLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.layer.cornerRadius = 5
+    }
     
 }
--- cinego/HomeViewController.swift
+++ cinego/HomeViewController.swift
@@ -11,20 +11,22 @@ import UIKit
 class HomeViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
     @IBOutlet weak var homeBannerSlider: UIScrollView!
+    @IBOutlet weak var upcomingMoviesCollectionView: UICollectionView!
+    @IBOutlet weak var cinemaTheatersCollectionView: UICollectionView!
     
     var imageBanners = [UIImage]()
     var upcomingMovies: [UIImage] = [#imageLiteral(resourceName: "indigo-160x240"), #imageLiteral(resourceName: "fuchsia-160x240"), #imageLiteral(resourceName: "lime-160x240"), #imageLiteral(resourceName: "maroon-160x240"), #imageLiteral(resourceName: "scarlet-160x240"), #imageLiteral(resourceName: "olive-160x240"), #imageLiteral(resourceName: "teal-160x240")]
+    var cinemaTheaters: [(location: String, image: UIImage)] = [
+        ("Melbourne CBD", #imageLiteral(resourceName: "cinema-image1")),
+        ("Fitzroy", #imageLiteral(resourceName: "cinema-image2")),
+        ("St. Kilda", #imageLiteral(resourceName: "cinema-image3")),
+        ("Sunshine", #imageLiteral(resourceName: "640x360"))
+    ]
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         loadHomeBannerSlider()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     // loads the home page slider banner
@@ -32,42 +34,54 @@ class HomeViewController: UIViewController, UICollectionViewDataSource, UICollec
         imageBanners = [#imageLiteral(resourceName: "cinema-image1") ,#imageLiteral(resourceName: "cinema-image2"), #imageLiteral(resourceName: "cinema-image3"), #imageLiteral(resourceName: "640x360")]
         
         for i in 0..<imageBanners.count {
-            
             let imageView = UIImageView()
             imageView.image = imageBanners[i]
             imageView.contentMode = .scaleAspectFill
-            
-            // images start from left edge of the screen; don't use [Int(self.view.frame.width) * i] because it will return an Int
             let xPos = self.view.frame.width * CGFloat(i)
-            
             imageView.frame = CGRect(x: xPos, y: 0, width: self.homeBannerSlider.frame.width, height: self.homeBannerSlider.frame.height)
-            
-            // total slides' width = banner width * number of images
             homeBannerSlider.contentSize.width = homeBannerSlider.frame.width * CGFloat(i + 1)
-            
-            // insert the images to the slider
             homeBannerSlider.addSubview(imageView)
-            
         }
     }
     
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return upcomingMovies.count
+        if collectionView == self.upcomingMoviesCollectionView {
+            return upcomingMovies.count
+        }
+        
+        if collectionView == self.cinemaTheatersCollectionView {
+            return self.cinemaTheaters.count
+        }
+        
+        return 0
     }
+    
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieCollectionViewCell", for: indexPath) as! MovieCollectionViewCell
+        if collectionView == self.upcomingMoviesCollectionView {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieCollectionViewCell", for: indexPath) as! MovieCollectionViewCell
+            cell.bannerIcon.image = upcomingMovies[indexPath.row]
+            cell.movieTitle.text = "Movie title: Something blah"
+            cell.movieReleaseYear.text = String(2017)
+            cell.movieAudienceType.text = "PG"
+            return cell
+        }
         
-        cell.bannerIcon.image = upcomingMovies[indexPath.row]
-        cell.movieTitle.text = "Movie title: Something blah"
-        cell.movieReleaseYear.text = String(2017)
-        cell.movieAudienceType.text = "PG"
+        if collectionView == self.cinemaTheatersCollectionView {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CinemaCollectionViewCell", for: indexPath) as! CinemaCollectionViewCell
+            cell.cinemaImage.image = cinemaTheaters[indexPath.row].image
+            cell.cinemaLabel?.text = cinemaTheaters[indexPath.row].location
+            return cell
+        }
         
-        return cell
-        
+        return UICollectionViewCell()
     }
     
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
     
 
     /*
