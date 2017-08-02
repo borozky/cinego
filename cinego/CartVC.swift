351be244ef77c9b831bff5bfbd7c865fcf42b0b7
Joshua Orozco <s3485376@student.rmit.edu.au>
2017-08-02 21:58:59 +1000

    Updated Cart view controller

---
+++ cinego/CartVC.swift
@@ -0,0 +1,32 @@
+//
+//  CartVC.swift
+//  cinego
+//
+//  Created by Victor Orosco on 2/8/17.
+//  Copyright © 2017 ISE Superstars. All rights reserved.
+//
+
+import UIKit
+
+class CartVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CartItemTableViewCell", for: indexPath)
+        cell.textLabel?.text = "12 July 09:30am"
+        cell.detailTextLabel?.text = "1 ticket"
+        return cell
+    }
+
+}
