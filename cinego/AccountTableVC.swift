c7fc4abc1dde155e2eb482302118c67c75cab4c0
何家红 <sarahe@hejiahongdeMacBook-Pro.local>
2017-08-07 09:46:34 +1000

    added user information section in the Account page

---
+++ cinego/AccountTableVC.swift
@@ -0,0 +1,39 @@
+//
+//  AccountTableVC.swift
+//  cinego
+//
+//  Created by 何家红 on 7/8/17.
+//  Copyright © 2017 ISE Superstars. All rights reserved.
+//
+
+import UIKit
+
+class AccountTableVC: UITableViewController {
+    private let tableViewCellID_UserInfo = "UserInformationTableViewCell"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: self.tableViewCellID_UserInfo, for: indexPath)
+        return cell
+    }
+    
+    
+}
