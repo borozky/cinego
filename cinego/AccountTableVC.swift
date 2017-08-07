eb30ce4daaa5c799de0eab6a175c853fcb4ec61f
Joshua Orozco <s3485376@b01409015-23.local>
2017-08-07 19:12:53 +1000

    updated storyboard and AccountTableVC

--- cinego/AccountTableVC.swift
+++ cinego/AccountTableVC.swift
@@ -9,12 +9,15 @@
 import UIKit
 
 class AccountTableVC: UITableViewController {
-    private let tableViewCellID_UserInfo = "UserInformationTableViewCell"
+    
+    private let tableViewCellIDs = ["UserInformationTableViewCell", "UserPastOrdersTableViewCell", "UserUpcomingMovieSessionsTableViewCell"]
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -22,16 +25,48 @@ class AccountTableVC: UITableViewController {
 
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        
-        return 1
+        return 2
     }
+    
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 1
+        }
         
-        return 1
+        if section > 0 {
+            return 2
+        }
+        
+        return 0
     }
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: self.tableViewCellID_UserInfo, for: indexPath)
+        var cell = UITableViewCell()
+        
+        // the user account information
+        if indexPath.section == 0 {
+            cell = tableView.dequeueReusableCell(withIdentifier: tableViewCellIDs[0], for: indexPath)
+            return cell
+        }
+        
+        if indexPath.section > 0 && indexPath.row == 0 {
+            cell = tableView.dequeueReusableCell(withIdentifier: tableViewCellIDs[1], for: indexPath)
+            tableView.estimatedRowHeight = 44
+            tableView.rowHeight = UITableViewAutomaticDimension
+            return cell
+        }
+        
+        if indexPath.section > 0 && indexPath.row == 1 {
+            cell = tableView.dequeueReusableCell(withIdentifier: tableViewCellIDs[2], for: indexPath)
+            tableView.estimatedRowHeight = 44
+            tableView.rowHeight = UITableViewAutomaticDimension
+            return cell
+        }
+        
+        
+        
         return cell
     }
     
