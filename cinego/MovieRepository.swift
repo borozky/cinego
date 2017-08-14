482b902fcdc58b5e7c7caa57d427a2e0a7e9dac2
何家红 <sarahe@hejiahongdeMacBook-Pro.local>
2017-08-14 12:16:11 +1000

    Added ids for sample movies

--- cinego/MovieRepository.swift
+++ cinego/MovieRepository.swift
@@ -15,16 +15,19 @@ class MovieRepository : IMovieRepository {
         
         
         var movie = Movie(title: "Alien Covenant", releaseDate: "19 May 2017", duration: 122, sessions: [], images: ["alien_covenant"])
+        movie.id = 1
         movie.details = "Bound for a remote planet on the far side of the galaxy, the crew of the colony ship 'Covenant' discovers what is thought to be an uncharted paradise, but is actually a dark, dangerous world – which has its sole inhabitant the 'synthetic', David, survivor of the doomed Prometheus expedition."
         movies.append(movie)
         
         
         movie = Movie(title: "Baby Driver", releaseDate: "11 March 2017", duration: 113, sessions: [], images: ["baby_driver"])
+        movie.id = 2
         movie.details = "After being coerced into working for a crime boss, a young getaway driver finds himself taking part in a heist doomed to fail."
         movies.append(movie)
         
         
         movie = Movie(title: "Boyka: Undisputed IV", releaseDate: "22 September 2017", duration: 87, sessions: [], images: ["boyka_undisputed_4"])
+        movie.id = 3
         movie.details = "In the fourth installment of the fighting franchise, Boyka is shooting for the big leagues when an accidental death in the ring makes him question everything he stands for. When he finds out the wife of the man he accidentally killed is in trouble, Boyka offers to fight in a series of impossible battles to free her from a life of servitude"
         movies.append(movie)
         
