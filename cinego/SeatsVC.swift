89807c1d07656281c2cb2ae3b4d6d28b2c56e3c7
Joshua Orozco <s3485376@student.rmit.edu.au>
2017-08-18 00:46:03 +1000

    Seats VC

---
+++ cinego/SeatsVC.swift
@@ -0,0 +1,17 @@
+//
+//  SeatsVC.swift
+//  cinego
+//
+//  Created by Victor Orosco on 16/8/17.
+//  Copyright © 2017 ISE Superstars. All rights reserved.
+//
+
+import UIKit
+
+class SeatsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
