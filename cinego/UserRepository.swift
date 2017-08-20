c427e97108e1c4675915cec76f170c07365ba39c
何家红 <sarahe@hejiahongdeMacBook-Pro.local>
2017-08-20 14:49:04 +1000

    UserRepository

--- cinego/UserRepository.swift
+++ cinego/UserRepository.swift
@@ -21,8 +21,21 @@ protocol IUserRepository {
 
 class UserRepository: IUserRepository {
     
+    private var users: [User] = [User (id:"1", email: "s3485376@student.rmit.edu.au", username: "s3485376", orders: []),
+                                  User (id:"2", email: "s3526309@student.rmit.edu.au", username: "s3526309", orders: []),
+                                  User (id:"3", email: "joushua@gmail.com", username: "Joushua", orders: []),
+                                  User (id:"4", email: "jiahonghe@gmial.com", username: "Jiahong", orders: []),
+                                  User (id:"5", email: "sara@gmail.com", username: "Sara", orders: []),
+                                  User (id:"6", email: "bob@gmail.com", username: "Bob", orders: []),
+                                  User (id:"7", email: "angie@gmai.com", username: "Angie", orders: []),
+                                  User (id:"8", email: "david@gmai.com", username: "David", orders: []),
+                                  User (id:"9", email: "helloword@gmail.com", username: "Hello", orders: []),
+                                  User (id:"10", email: "connie@gmai.com", username: "Connie", orders: [])
+                                 ]
+
     func find(byId id: Int) -> User? {
-        return nil
+     
+        return nil;
     }
     
     func find(byUsername username: String) -> User? {
