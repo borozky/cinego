0297d454061760cc92737915bd2e8bececb96b04
Josh MacDev <joshmacdev@victor-oroscos-imac.local>
2017-09-24 00:11:03 +1000

    cinema service test file added

---
+++ cinegoTests/ServicesTests/CinemaServiceTests.swift
@@ -0,0 +1,21 @@
+//
+//  CinemaServiceTests.swift
+//  cinego
+//
+//  Created by Josh MacDev on 23/9/17.
+//  Copyright © 2017 ISE Superstars. All rights reserved.
+//
+
+import XCTest
+
+class CinemaServiceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+}
