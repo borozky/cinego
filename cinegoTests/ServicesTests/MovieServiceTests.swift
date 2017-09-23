5600c421263c4652443ead6b04515acd8d1a5b7f
Josh MacDev <joshmacdev@victoroscosimac.gateway>
2017-09-24 00:08:24 +1000

    Movie service test file

---
+++ cinegoTests/ServicesTests/MovieServiceTests.swift
@@ -0,0 +1,28 @@
+//
+//  MovieServiceTests.swift
+//  cinego
+//
+//  Created by Josh MacDev on 23/9/17.
+//  Copyright © 2017 ISE Superstars. All rights reserved.
+//
+
+import XCTest
+import PromiseKit
+@testable import cinego
+
+class MovieServiceTests: XCTestCase {
+    
+    var movieService: IMovieService!
+    
+    override func setUp() {
+        super.setUp()
+        
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    
+    
+}
